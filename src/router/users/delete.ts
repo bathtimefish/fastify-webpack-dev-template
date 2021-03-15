f5e2773e5cb15e6e4efc9dc03ed3af993f2c3206
bathtimefish <muraoka@bathtimefish.com>
2021-03-15 22:12:03 +0900

    fixed a bug which could not find user by id

--- src/router/users/delete.ts
+++ src/router/users/delete.ts
@@ -7,16 +7,16 @@ const users: IServer.user[] = dummyResponse as any;
 export const deleteHandler: RouteHandlerMethod = async (req, rep) => {
   try {
     const p = req.params as any;
-    const user = users.find((u) => { return u.id === p.userId; });
+    const user = users.find((u) => { return u.id.toString() === p.userId; });
+    console.log(user);
     if (!user) throw boom.notFound();
     const deleted: IServer.user[] = [];
     for (const u of users) {
-      if (u.id === p.userId) continue;
-      deleted.push(u);
+      if (u.id.toString() !== p.userId) deleted.push(u);
     }
     console.info('Deleted Users', JSON.stringify(deleted));
     rep.status(204).send();
   } catch (e) {
-    throw boom.internal(e);
+    throw e;
   }
 };
